c496b1c71b67d20b9976ae341fdf538ecb1ae439
Eduarda Taschetto <eduarda.b.taschetti@gmail.com>
2021-01-15 19:29:51 -0300

    adicionando models

--- AvaliacaoDiaria/models.py
+++ AvaliacaoDiaria/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class AvaliacaoDiaria(models.Model):
+    SINTOMAS_CHOICES = (
+        ('dorDeCabeca', 'Dor de Cabeça'),
+        ('dorNoCorpo', 'Dor no corpo'),
+        ('cansaco', 'Cansaço'),
+        ('dorNoEstomago', 'Dor no estômago'),
+        ('inchaco', 'Inchaço'),
+        ('nausea', 'Náusea'),
+        ('enjoo', 'Enjôo'),
+        ('faltaDeAr', 'Falta de ar'),
+        ('ansiedade', 'Ansiedade'),
+        ('outro', 'Outro'),
+    )
+    sintomas = models.CharField('Sintomas', choices=SINTOMAS_CHOICES, max_length=15)
+    observacoes = models.TextField('Observações') #verificar se precisa max_length
+    # chave estrangeira para pessoa
+
