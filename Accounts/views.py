c355971f8b0a0475aca3169581694161c49b03b5
eduardataschetto <eduardataschettob@gmail.com>
2021-01-29 16:22:11 -0300

    editando perfil

--- Accounts/views.py
+++ Accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 from CadastroDePessoa.forms import UsuarioForm
+from CadastroDePessoa.models import Usuario
 
 
 def login(request):
@@ -31,25 +32,25 @@ def cadastro(request):
         return render(request, 'accounts/cadastro.html')
 
     form = UsuarioForm(request.POST or None)
+
     nome = request.POST.get("nome")
     email = request.POST.get('email')
     usuario = request.POST.get('usuario')
     senha = request.POST.get('senha')
-    
-    user = User.objects.create_user(username=usuario, email=email, password=senha, first_name=nome, last_name=nome)
-    user.save()
 
+    foto = request.FILES.get('foto')
+    nascimento = request.POST.get('nascimento')
+    sexo =  request.POST.get('sexo')
+    telefone = request.POST.get('telefone')
+    
 
     if form.is_valid():
-        print('form valid')
-        form.save()
-        messages.info(request, 'Usuário cadastrado com sucesso!')
+        user = User.objects.create_user(username=usuario, email=email, password=senha, first_name=nome, last_name=nome)
+        user.save()
+        usuario_db = Usuario(nome=nome, foto=foto, nascimento=nascimento, sexo=sexo, telefone=telefone, email=email, id_fk_cadastro_user=user)
+        usuario_db.save()
     else:
         print('form invalid')
-        messages.info(request, 'Erro ao cadastrar usuário!')
-
-  
-
 
     return redirect('index_login')
 
--- medfile/urls.py
+++ medfile/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', include('AvaliacaoDiaria.urls')),
     path('', include('Perfil.urls')),
@@ -32,4 +35,4 @@ urlpatterns = [
 
 
     path('admin/', admin.site.urls),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
