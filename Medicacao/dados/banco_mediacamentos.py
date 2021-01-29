17610d3f753ff8429dfd2714f092ca3d332f1e86
AmandaRH07 <amandarafahass13@gmail.com>
2021-01-29 17:20:26 -0300

    Banco

--- Medicacao/dados/banco_mediacamentos.py
+++ Medicacao/dados/banco_mediacamentos.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-leitura_csv = pd.read_csv("dados_medicamento.csv", sep=";", encoding="latin1",error_bad_lines=False)
+leitura_csv = pd.read_csv("C:/Users/Usuario/Desktop/TCC_Curso/Medicacao/dados/dados_medicamento.csv", sep=";", encoding="latin1",error_bad_lines=False)
 
-leitura_csv[:3]
+print(leitura_csv['NOME'][0])
 
-for i in leitura_csv:
-    print(i)
+# for i in leitura_csv:
+#     print(i)
