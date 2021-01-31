01b0ddde3587ddeb336e7d768771ee1d9b394e93
karla <karlapollad@gmail.com>
2021-01-30 23:50:02 -0300

    atualizações telas 30-01-2021

--- Historico/static/js/historico.js
+++ Historico/static/js/historico.js
@@ -1,19 +1,20 @@
-var $input    = document.getElementById('selecao-arquivo-receita');
+let $input = document.getElementById('selecao-arquivo-receita');
     
-    $fileName = document.getElementById('file-name');
+  $fileName = document.getElementById('file-name');
     
 
 $input.addEventListener('change', function(){
   $fileName.textContent = this.value;
 })
 
-  var $input2    = document.getElementById('selecao-arquivo-atestado');
-    
-  $fileName2 = document.getElementById('file-name2');
+
+let $input2 = document.getElementById('selecao-arquivo-atestado');
   
+  $fileName2 = document.getElementById('file-name2');
+
 
 $input2.addEventListener('change', function(){
 $fileName2.textContent = this.value;
 }
 );
-
+ 
--- Sangue/static/js/sangue.js
+++ Sangue/static/js/sangue.js
@@ -9,3 +9,4 @@ for (let i = 0; i <= btn.length; i++) {
     });
 }
 
+ 
