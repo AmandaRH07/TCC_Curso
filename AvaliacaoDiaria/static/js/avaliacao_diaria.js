8b87ebd55993ac8e3944171972c8a706c9ce8572
unknown <Python@BUE302D19.alunos.sc.senac.br>
2021-02-04 17:20:00 -0300

    att Tela Avaliação Diaria

--- AvaliacaoDiaria/static/js/avaliacao_diaria.js
+++ AvaliacaoDiaria/static/js/avaliacao_diaria.js
@@ -1,11 +1,15 @@
-function mostrar(elemento){
-    let display = document.getElementById(elemento).style.display;
-      if(display == "none"){
-          document.getElementById(elemento).style.display = 'block';
-          document.getElementById('outro').style.backgroundColor = '#ED553B'
-      }else{
-          document.getElementById(elemento).style.display = 'none';
-          document.getElementById('outro').style.backgroundColor = '#FCFFF5'
-      }    
-  }
+let btnOutro = document.getElementById('outro');
+let txtOutroSintoma = document.getElementById('txt-outro-sintoma');
 
+btnOutro.addEventListener('click', function(e) {
+    
+    e.preventDefault();
+    if (txtOutroSintoma.style.display == 'none') {
+        txtOutroSintoma.style.display = 'block';
+        btnOutro.style.backgroundColor = '#ED553B';
+    } else {
+        txtOutroSintoma.style.display = 'none';
+        btnOutro.style.backgroundColor = '#FCFFF5';
+    }
+
+});
