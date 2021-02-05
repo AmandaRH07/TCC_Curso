378cdadb990bc22f72e7a07743e68e28d7cf2a7a
Luana Matos Brito Sales <luanabritoms@gmail.com>
2021-02-04 22:01:24 -0300

    Render - Histórico de consultas e Doenças existentes

--- DoencasExistentes/views.py
+++ DoencasExistentes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponseRedirect
+from .models import DoencasExistentes
 from CadastroDePessoa.models import Usuario
 
 from .forms import DoencasExistentesForm
@@ -8,10 +9,13 @@ from .forms import DoencasExistentesForm
 @login_required(redirect_field_name='index_login')
 def doencas_existentes(request):
     usuario = Usuario.objects.get(id_fk_cadastro_user=request.user)
-    form = DoencasExistentesForm(request.POST, request.FILES or None)
+    dados_doencas_existentes = DoencasExistentes.objects.filter(fk_usuario_doencas_existentes=usuario.id_usuario)
 
     if str(request.method) == 'POST':
+        print(dados_doencas_existentes)
+        form = DoencasExistentesForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            return HttpResponseRedirect("doencas-existentes")
         
-    return render(request, "doencas-existentes.html", {"usuario": usuario})
+    return render(request, "doencas-existentes.html", {"usuario": usuario, 'dados_doencas_existentes':dados_doencas_existentes})
--- Historico/views.py
+++ Historico/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from .forms import HistoricoConsultasForm
-
+from django.http import HttpResponseRedirect
 from CadastroDePessoa.models import Usuario
+from .models import HistoricoConsultas
 
 
 def historico_consultas(request):
     usuario = Usuario.objects.get(id_fk_cadastro_user=request.user)
     form = HistoricoConsultasForm(request.POST, request.FILES or None)
+    dados_historico = HistoricoConsultas.objects.filter(fk_usuario_historico_consulta=usuario.id_usuario)
+
     if str(request.method) == 'POST' or str(request.method) == 'FILES':
+        form = HistoricoConsultasForm(request.POST)
         if form.is_valid():
             form.save()
+            return HttpResponseRedirect("historico")
         else: 
             print("invalido")
             print(form)
 
-    return render(request, "historico.html", {"form": form, 'dados_user': usuario})
+    return render(request, "historico.html", {"form": form, 'dados_user': usuario, 'dados_historico': dados_historico})
