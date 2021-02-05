8ee1d05088ddb0fb29f0eda1d27fd8c53c4ada77
AmandaRH07 <amandarafahass13@gmail.com>
2021-02-04 22:11:59 -0300

    render Vacinacao e Medicacao

--- Medicacao/views.py
+++ Medicacao/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
-from CadastroDePessoa.models import Usuario
-
 from .forms import MedicacaoForm
+from .models import Medicamentos
+
+from CadastroDePessoa.models import Usuario
 
 @login_required(redirect_field_name='index_login')
 def medicacao(request):
     usuario = Usuario.objects.get(id_fk_cadastro_user=request.user)
-    form = MedicacaoForm(request.POST, request.FILES or None)
+    dados_medicamentos = Medicamentos.objects.filter(fk_user_medicacao=usuario.id_usuario)
 
     if str(request.method) == 'POST':
+        form = MedicacaoForm(request.POST, request.FILES or None)
         if form.is_valid():
             form.save()
-        
-    return render(request, "medicacao.html", {"usuario": usuario})
+            return HttpResponseRedirect("medicacao")
+    return render(request, "medicacao.html", {"usuario": usuario, 'dados_medicamentos': dados_medicamentos})
--- Vacinas/views.py
+++ Vacinas/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
 from .forms import VacinasForm
+from .models import Vacinas
 
 from CadastroDePessoa.models import Usuario
 
 def vacinas(request):
     usuario = Usuario.objects.get(id_fk_cadastro_user=request.user.id)
-    form = VacinasForm(request.POST or None)
-    if str(request.method) == 'POST' or str(request.method) == 'FILES':
+    dados_vacinas = Vacinas.objects.filter(fk_usuario_vacinas=usuario.id_usuario)
+
+    if str(request.method) == 'POST':
+        form = VacinasForm(request.POST)
         if form.is_valid():
-            print("oi")
             form.save()
-    return render(request, "vacinas.html", {"form": form, 'dados_user': usuario})
+            return HttpResponseRedirect("vacinas")
+    return render(request, "vacinas.html", {'dados_user': usuario, 'dados_vacinas': dados_vacinas})
