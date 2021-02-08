6fb5cfa913fe76611571d3edbbb653de0867170b
aka-luana <Python@BUE302D16.alunos.sc.senac.br>
2021-02-08 14:54:24 -0300

    Update - Histórico familiar

--- Historico/views.py
+++ Historico/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .forms import HistoricoConsultasForm
+from .forms import HistoricoConsultasForm, HistoricoFamiliarForm
 from django.http import HttpResponseRedirect
 from CadastroDePessoa.models import Usuario
-from .models import HistoricoConsultas
+from .models import HistoricoConsultas, HistoricoFamiliar
 
 from django.contrib.auth.decorators import login_required
 
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 def historico_consultas(request):
     usuario = Usuario.objects.get(id_fk_cadastro_user=request.user)
     dados_historico = HistoricoConsultas.objects.filter(fk_usuario_historico_consulta=usuario.id_usuario)
+    dados_familiar = HistoricoFamiliar.objects.filter(fk_usuario_historico_familiar=usuario.id_usuario)
 
     if str(request.method) == 'POST' or str(request.method) == 'FILES':
         form = HistoricoConsultasForm(request.POST, request.FILES)
@@ -17,4 +18,24 @@ def historico_consultas(request):
             form.save()
             return HttpResponseRedirect("historico")
 
-    return render(request, "historico.html", {'dados_user': usuario, 'dados_historico': dados_historico})
+    return render(request, "historico.html", {'dados_user': usuario, 'dados_historico': dados_historico, 'dados_familiar': dados_familiar})
+
+@login_required(redirect_field_name='index_login')
+def historico_familiar(request):
+    print("entrou")
+    usuario = Usuario.objects.get(id_fk_cadastro_user=request.user)
+    dados_familiar = HistoricoFamiliar.objects.filter(fk_usuario_historico_familiar=usuario.id_usuario)
+
+    if str(request.method) == 'POST':
+        form = HistoricoFamiliarForm(request.POST)
+        if form.is_valid():
+            print("eh valido")
+            form.save()
+            return HttpResponseRedirect("historico")
+        else:
+            print("n eh valido")
+            print(form)
+    else:
+        print("n eh post")
+
+    return redirect('historico')
