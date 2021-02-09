0384d63747cac2eb3a5d7ef7714817d6f68e7c8c
Amanda Rafaela Hass <61278890+AmandaRH07@users.noreply.github.com>
2021-02-09 14:10:06 -0300

    Delete AlergiaIntolerancia directory

--- AlergiaIntolerancia/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('alergia-intolerancia', TemplateView.as_view(template_name='alergia-intolerancia.html'), name='alergia-intolerancia'),
-]
