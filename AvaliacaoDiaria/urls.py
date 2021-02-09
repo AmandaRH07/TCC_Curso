ee0555d636be1ab72053da77882020690fc1f37d
unknown <Python@BUE302D17.alunos.sc.senac.br>
2021-02-09 14:12:35 -0300

    alergia e intolerancia delete

--- AvaliacaoDiaria/urls.py
+++ AvaliacaoDiaria/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('avaliacao-diaria', views.index, name='index')
-]
+    path('avaliacao-diaria', views.index, name='index'),
+    path('', views.index),
+] 
