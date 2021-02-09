5e430c65dc0a71c567209bb0f2040a0ede2051dc
Eduarda Taschetto <eduarda.b.taschetti@gmail.com>
2021-02-09 10:06:21 -0300

    historico - editando e excluindo

--- Historico/urls.py
+++ Historico/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('historico', views.historico_consultas, name='historico'),
+    path('historico/<int:pk>', views.historico_consultas_detail, name='historico_consultas_detail'),
+    path('historico-consulta-delete/<int:pk>', views.historico_consultas_delete, name='historico_consultas_delete'),
+
+
     path('historico_familiar', views.historico_familiar, name='historico-familiar'),
 ]
