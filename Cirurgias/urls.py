d527b6dd2fd7b34f7221f20d3212dacd5ae6cfe8
Luana Matos Brito Sales <luanabritoms@gmail.com>
2021-02-08 22:08:04 -0300

    Cirurgias - Editar com erro e excluir funcionando

--- Cirurgias/urls.py
+++ Cirurgias/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import cirurgia
+from .views import cirurgia, cirurgia_detail, cirurgia_delete
 
 urlpatterns = [
-    path('cirurgias', cirurgia, name='cirurgias')
+    path('cirurgias', cirurgia, name='cirurgias'),
+    path('cirurgia/<int:pk>', cirurgia_detail, name="cirurgia_detail"),
+    path('cirurgia-delete/<int:pk>', cirurgia_delete, name="cirurgia_delete"),
 ]
