7ce9716d5c8eb95d03b5bf1a6dabe8082b22e369
Eduarda Taschetto <eduarda.b.taschetti@gmail.com>
2021-02-13 22:07:18 -0300

    formatando cod

--- Cirurgias/views.py
+++ Cirurgias/views.py
@@ -1,52 +1,54 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from .forms import CirurgiasForm
 from .models import Cirurgias
+
 from CadastroDePessoa.models import Usuario
 
-from django.contrib.auth.decorators import login_required
+
 
 @login_required(redirect_field_name='index_login')
 def cirurgia(request):
-    # usuario = Usuario.objects.get(id_fk_cadastro_user=request.user.id)
     usuario = get_object_or_404(Usuario, id_fk_cadastro_user=request.user)
-
     dados_cirurgias = Cirurgias.objects.filter(fk_usuario_cirurgias=usuario.id_usuario)
 
     if str(request.method) == 'POST':
         form = CirurgiasForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("cirurgias")
+            return redirect('cirurgias')
             
-    return render(request, "cirurgias.html", {'dados_user': usuario, 'dados_cirurgias': dados_cirurgias})
+    return render(request, "cirurgias.html",
+        {
+        'dados_user': usuario,
+        'dados_cirurgias': dados_cirurgias
+        })
+
 
 @login_required(redirect_field_name='index_login')
 def cirurgia_detail(request, pk):
-    # usuario = Usuario.objects.get(id_fk_cadastro_user=request.user)
     usuario = get_object_or_404(Usuario, id_fk_cadastro_user=request.user)
-
     dados_cirurgias = Cirurgias.objects.filter(fk_usuario_cirurgias=usuario.id_usuario)
 
-    # cirurgia_detail = Cirurgias.objects.get(id_cirugias=pk)
     cirurgia_detail = get_object_or_404(Cirurgias, id_cirugias=pk)
-
 
     if str(request.method) == 'POST':
         form = CirurgiasForm(request.POST, instance=cirurgia_detail)
         if form.is_valid():
             form.save()
             return redirect('cirurgias')
-        else:
-            print(form)
 
-    return render(request, "cirurgias.html", {"dados_user": usuario, 'dados_cirurgias': dados_cirurgias, 'cirurgia_detail': cirurgia_detail})
+    return render(request, "cirurgias.html",
+        {
+            "dados_user": usuario,
+            'dados_cirurgias': dados_cirurgias,
+            'cirurgia_detail': cirurgia_detail
+        })
 
 
 @login_required(redirect_field_name='index_login')
 def cirurgia_delete(request, pk):
-    # cirurgia = Cirurgias.objects.get(id_cirugias=pk)
     cirurgia_detail = get_object_or_404(Cirurgias, id_cirugias=pk)
     cirurgia_detail.delete()
 
--- Sangue/views.py
+++ Sangue/views.py
@@ -1,32 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import TipoSanguineoForm
- 
-from CadastroDePessoa.models import Usuario
-from Sangue.models import TipoSanguineo
- 
- 
 from django.contrib.auth.decorators import login_required
+
+from CadastroDePessoa.models import Usuario
+
+from .forms import TipoSanguineoForm
+from .models import TipoSanguineo
+
  
 @login_required(redirect_field_name='index_login')
 def sangue(request):
-    # usuario = Usuario.objects.get(id_fk_cadastro_user=request.user.id)
     usuario = get_object_or_404(Usuario, id_fk_cadastro_user=request.user.id)
     tipo_sanguineo = TipoSanguineo.objects.filter(fk_usuario_tipo_sanguineo=usuario.id_usuario)
- 
-    form = TipoSanguineoForm(request.POST or None)
     
     if request.method == 'POST':
+        form = TipoSanguineoForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('sangue')
- 
-    return render(request, "sangue.html", {'dados_user': usuario, 'tipo_sanguineo': tipo_sanguineo})
+
+    return render(request, "sangue.html",
+        {
+            'dados_user': usuario,
+            'tipo_sanguineo': tipo_sanguineo
+        })
  
 
 @login_required(redirect_field_name='index_login')
 def sangue_detail(request):
     usuario = get_object_or_404(Usuario, id_fk_cadastro_user=request.user)
- 
     tipo_sanguineo = TipoSanguineo.objects.filter(fk_usuario_tipo_sanguineo=usuario.id_usuario)
  
     if str(request.method) == 'POST':
@@ -35,4 +36,8 @@ def sangue_detail(request):
             form.save()
             return redirect('sangue')
  
-    return render(request, "sangue.html", {'dados_user': usuario, 'tipo_sanguineo': tipo_sanguineo})
+    return render(request, "sangue.html", 
+        {
+            'dados_user': usuario,
+            'tipo_sanguineo': tipo_sanguineo
+        })
